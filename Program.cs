df87a111cec7a5006622da8be0a8e20891984091
burak <burak@BURAK>
2024-08-19 11:25:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> coffeeList = new List<string>();
+        string userCoffeeInfo;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($" Enter Coffee {i + 1} : ");
+            userCoffeeInfo = Console.ReadLine();
+            coffeeList.Add(userCoffeeInfo);
+        }
+
+        Console.WriteLine("Coffee Name List : ");
+        foreach (string coffee in coffeeList)
+        {
+            Console.WriteLine(coffee);
+        }
+    }
+}
